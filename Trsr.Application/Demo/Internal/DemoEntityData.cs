e4f102951a0f90be6920ade51130ad32134ed131
everharder <mail@gehirn.cloud>
2026-05-07 13:22:20 +0200

    remove demo data seeder

--- Trsr.Application/Demo/Internal/DemoEntityData.cs
+++
@@ -1,8 +0,0 @@
-using Trsr.Domain;
-
-namespace Trsr.Application.Demo.Internal;
-
-internal record DemoEntityData(
-    Guid Id,
-    DateTimeOffset CreatedAt,
-    DateTimeOffset UpdatedAt) : IDomainEntityData;
