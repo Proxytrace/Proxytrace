77a8b75295e0e1aae15041f7db6bf565060122a0
JabbaKadabra <daniel@gehirn.cloud>
2026-08-29 19:29:39 +0200

    remove license gate

--- Proxytrace.Licensing/TierDefinition.cs
+++
@@ -1,8 +0,0 @@
-namespace Proxytrace.Licensing;
-
-/// <summary>
-/// The feature set and limits granted by a particular license tier.
-/// </summary>
-public sealed record TierDefinition(
-    IReadOnlySet<LicenseFeature> Features,
-    IReadOnlyDictionary<LicenseLimit, long> Limits);
